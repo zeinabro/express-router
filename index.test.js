223a3cd2d7d72daa4b21cc27ee86142e95dccc5f
Zeinab Roumieh <zeinabroumieh@outlook.com>
2024-05-21 16:40:34 +0100

    fruits route tests

--- index.test.js
+++ index.test.js
@@ -65,3 +65,62 @@ describe("/users endpoint", () => {
         expect(res.text).toEqual("User deleted successfully")
     })
 })
+
+describe("/fruits endpoint", () => {
+    test("get all fruits", async() => {
+        const res = await request(app).get("/fruits")
+        expect(res.statusCode).toBe(200)
+        expect(res.body[0].name).toEqual(seedFruits[0].name)
+        expect(res.body.length).toBe(seedFruits.length)
+    })
+
+    test("get fruit by id", async() => {
+        const res = await request(app).get("/fruits/1")
+        expect(res.statusCode).toBe(200)
+        expect(res.body.name).toEqual(seedFruits[0].name)
+    })
+
+    test("create new fruit with valid info", async() => {
+        const fruit = {
+            name: "Fruit",
+            color: "color"
+        }
+        const res = await request(app).post("/fruits").send(fruit)
+        expect(res.statusCode).toBe(200)
+        expect(res.body.name).toEqual(fruit.name)
+    })
+
+    test("cannot create new fruit with invalid info", async() => {
+        const fruit = {
+            name: "fruit",
+            color: ""
+        }
+        const res = await request(app).post("/fruits").send(fruit)
+        expect(res.statusCode).toBe(400)
+        expect(res.body.error[0]).toEqual(
+            {
+                "value": "",
+                "msg": "Invalid value",
+                "param": "color",
+                "location": "body"
+            }
+        )
+    })
+
+    test("update existing fruit", async() => {
+        const fruit = {
+            name: "Testing",
+            color: "test"
+        }
+        const res = await request(app).put("/fruits/4").send(fruit)
+        expect(res.statusCode).toBe(200)
+        expect(res.body.name).toEqual(fruit.name)
+        expect(res.body.color).toEqual(fruit.color)
+    })
+
+    test("delete fruit", async() => {
+        const res = await request(app).delete("/fruits/4")
+        expect(res.statusCode).toBe(200)
+        expect(res.text).toEqual("Fruit deleted successfully")
+    })
+})
