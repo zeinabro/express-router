7dff6908ed9b162d892b851d217fd4e64f9dbec2
Zeinab Roumieh <zeinabroumieh@outlook.com>
2024-05-21 16:36:00 +0100

    fruits router

---
+++ routes/fruits.js
@@ -0,0 +1,66 @@
+const express = require("express")
+const Fruit = require("../models/Fruit")
+const { check, validationResult } = require("express-validator")
+
+const fruitRouter = express.Router()
+fruitRouter.use(express.json())
+fruitRouter.use(express.urlencoded())
+
+fruitRouter.get("/", async(req,res) => {
+    const fruits = await Fruit.findAll();
+    res.json(fruits)
+})
+
+fruitRouter.get("/:id", async(req,res) => {
+    const fruit = await Fruit.findByPk(req.params.id)
+    res.json(fruit)
+})
+
+const validator = [
+    check("color").trim().not().isEmpty()
+]
+
+fruitRouter.post("/",validator, async(req,res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()){
+        res.status(400).json({ error: errors.array() })
+    } else {
+       const fruit = await Fruit.create({
+        name: req.body.name,
+        color: req.body.color
+       })
+       res.json(fruit)
+    }
+})
+
+fruitRouter.put("/:id", validator, async(req,res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()){
+        res.status(400).json({ error: errors.array() })
+    } else {
+       const fruit = await Fruit.update(
+        {
+            name: req.body.name,
+            color: req.body.color
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        })
+       res.json(fruit)
+    }
+})
+
+fruitRouter.delete("/:id", async(req,res) => {
+    await Fruit.destroy(
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    )
+    res.send("Fruit deleted successfully")
+})
+
+module.exports = fruitRouter
--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express()
 const usersRouter = require("../routes/users");
+const fruitRouter = require("../routes/fruits");
 
 app.use("/users", usersRouter)
+app.use("/fruits", fruitRouter)
 
 module.exports = app;
