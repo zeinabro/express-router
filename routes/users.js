28a8cda91c7758ce3c8d653693a246acfed93250
Zeinab Roumieh <zeinabroumieh@outlook.com>
2024-05-21 09:46:09 +0100

    users routes

---
+++ routes/users.js
@@ -0,0 +1,52 @@
+const express = require("express")
+const User = require("../models/User")
+
+const usersRouter = express.Router()
+
+usersRouter.get("/", async(req,res) => {
+    const users = await User.findAll()
+    res.json(users)
+})
+
+usersRouter.get("/:id", async(req,res) => {
+    const user = await User.findByPk(req.params.id)
+    res.json(user)
+})
+
+usersRouter.post("/", async(req,res) => {
+    const data = req.body
+    const user = await User.create({
+        name: data.name,
+        age: data.age
+    })
+    res.json(user)
+})
+
+usersRouter.put("/:id", async(req,res) => {
+    await User.update(
+        {
+            name: req.body.name,
+            age: req.body.age
+        },
+        {
+            where:{
+                id: req.params.id
+            }
+        }
+    )
+    const user = await User.findByPk(req.params.id)
+    res.json(user)
+})
+
+usersRouter.delete("/:id", async(req,res) => {
+    await User.destroy(
+        {
+            where:{
+                id: req.params.id
+            }
+        }
+    )
+    res.send("User deleted successfully")
+})
+
+module.exports = usersRouter
